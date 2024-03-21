bf90db0db36b8651e4f06630131b8d98f917a89f
CIT195 <122820747+Holmes1776@users.noreply.github.com>
2024-03-21 14:40:17 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace lambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Func<double, double, double> add = (x, y) => x + y;
+            Func<double, double, double> mult = (x, y) => x * y;
+
+            Console.WriteLine($"Addition calculation: {add(num1, num2)}");
+            Console.WriteLine($"Multiplication calculation: {mult(num1, num2)}");
+
+            Func<double, double, double> lesser = (x, y) =>
+            {
+                if (x < y)
+                {
+                    return x;
+                }
+                else
+                    return y;
+            };
+            Console.WriteLine($"Smaller number: {lesser(num1, num2)}");
+        }
+    }
+}
